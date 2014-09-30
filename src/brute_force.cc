23e2ffceeb08e7e98abb6513ef343419a2dd8697
Martin Kersner <m.kerner@gmail.com>
2014-09-30 14:13:00 +0200

    first commit

---
+++ src/brute_force.cc
@@ -0,0 +1,19 @@
+/**
+ * KNAPSACK PROBLEM - bruteforce solution
+ * @author Martin Kersner, m.kersner@gmail.com
+ * @date 09/30/2014
+ * @last_update 09/30/2014
+ */
+
+#include <iostream>
+
+#include "read_instance.h"
+
+int main (int arcg, char** argv) {
+    
+    // readfile
+    // create structure
+    // compute bute force
+    // print to output
+    return 0;
+}
---
+++ src/read_instance.h
@@ -0,0 +1,7 @@
+/**
+ *
+ */
+class ReadInstance {
+    public:
+    private:
+};
