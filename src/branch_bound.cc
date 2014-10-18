8a88db5c1be90c493d373d6d4d13bf1522d5b2fe
Martin Kersner <m.kerner@gmail.com>
2014-10-18 19:24:16 +0200

    added method branch & bound for solving knapsack problem; tested until 15 items; no memory leaks

---
+++ src/branch_bound.cc
@@ -0,0 +1,27 @@
+/**
+ * @file
+ * @author Martin Kersner <m.kersner@gmail.com>
+ * @date 10/18/2014
+ *
+ * @section DESCRIPTION
+ *
+ * KNAPSACK PROBLEM 
+ *
+ * Brute force solution.
+ */
+
+#include "branch_bound.h"
+
+int main(int argc, char** argv) {
+    
+    if (argc >= MIN_PARAM) {
+        //char * file_name = argv[1];
+        //auto inst = new Instances(file_name);
+        //SolveBruteForce(inst);
+        //inst->PrintSolutions();
+
+        //delete inst;
+    }
+
+    return EXIT_SUCCESS;
+}
---
+++ src/branch_bound.h
@@ -0,0 +1,14 @@
+/**
+ * @file
+ * @author Martin Kersner <m.kersner@gmail.com>
+ * @date 09/30/2014
+ */
+
+#ifndef BRANCHBOUND_HH
+#define BRANCHBOUND_HH
+
+#define MIN_PARAM 2
+
+#include <stdlib.h>
+
+#endif //BRANCHBOUND_HH
